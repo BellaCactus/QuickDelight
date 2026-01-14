c6564e3f1dfa088c3ce77e8cf2ff40e882a1e8f1
Bella <84885135+BellaCactus@users.noreply.github.com>
2026-01-15 08:15:50 +1300

    Add files via upload

---
+++ 02-Automated Test/QuickDelightOrderForm v1.2/QuickDelightOrderForm/Form1.Designer.vb
@@ -0,0 +1,351 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        lblTime = New Label()
+        Timer1 = New Timer(components)
+        txtCustomerName = New TextBox()
+        txtAddress = New TextBox()
+        dtpOrderDate = New DateTimePicker()
+        cmbMeal = New ComboBox()
+        txtQuantity = New TextBox()
+        txtPrice = New TextBox()
+        btnAdd = New Button()
+        btnDelete = New Button()
+        dgvOrder = New DataGridView()
+        colMeal = New DataGridViewTextBoxColumn()
+        colQty = New DataGridViewTextBoxColumn()
+        colPrice = New DataGridViewTextBoxColumn()
+        colSubtotal = New DataGridViewTextBoxColumn()
+        lblTotal = New Label()
+        lblGST = New Label()
+        lblToPay = New Label()
+        btnExit = New Button()
+        GroupBox1 = New GroupBox()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        GroupBox2 = New GroupBox()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        CType(dgvOrder, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 16F, FontStyle.Bold)
+        Label1.Location = New Point(132, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(289, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "QuickDelight - Order Form"
+        ' 
+        ' lblTime
+        ' 
+        lblTime.AutoSize = True
+        lblTime.Location = New Point(427, 21)
+        lblTime.Name = "lblTime"
+        lblTime.Size = New Size(49, 15)
+        lblTime.TabIndex = 1
+        lblTime.Text = "00:00:00"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 1000
+        ' 
+        ' txtCustomerName
+        ' 
+        txtCustomerName.Location = New Point(124, 63)
+        txtCustomerName.Name = "txtCustomerName"
+        txtCustomerName.Size = New Size(200, 23)
+        txtCustomerName.TabIndex = 2
+        ' 
+        ' txtAddress
+        ' 
+        txtAddress.Location = New Point(124, 92)
+        txtAddress.Multiline = True
+        txtAddress.Name = "txtAddress"
+        txtAddress.Size = New Size(200, 23)
+        txtAddress.TabIndex = 3
+        ' 
+        ' dtpOrderDate
+        ' 
+        dtpOrderDate.Location = New Point(124, 124)
+        dtpOrderDate.Name = "dtpOrderDate"
+        dtpOrderDate.Size = New Size(200, 23)
+        dtpOrderDate.TabIndex = 4
+        ' 
+        ' cmbMeal
+        ' 
+        cmbMeal.FormattingEnabled = True
+        cmbMeal.Items.AddRange(New Object() {"Pizza", "Burger", "Salad", "Sushi"})
+        cmbMeal.Location = New Point(13, 43)
+        cmbMeal.Name = "cmbMeal"
+        cmbMeal.Size = New Size(121, 23)
+        cmbMeal.TabIndex = 5
+        ' 
+        ' txtQuantity
+        ' 
+        txtQuantity.Location = New Point(149, 43)
+        txtQuantity.Name = "txtQuantity"
+        txtQuantity.Size = New Size(100, 23)
+        txtQuantity.TabIndex = 6
+        ' 
+        ' txtPrice
+        ' 
+        txtPrice.Location = New Point(278, 43)
+        txtPrice.Name = "txtPrice"
+        txtPrice.Size = New Size(100, 23)
+        txtPrice.TabIndex = 7
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(13, 84)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 23)
+        btnAdd.TabIndex = 8
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(114, 84)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(75, 23)
+        btnDelete.TabIndex = 9
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' dgvOrder
+        ' 
+        dgvOrder.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvOrder.Columns.AddRange(New DataGridViewColumn() {colMeal, colQty, colPrice, colSubtotal})
+        dgvOrder.Location = New Point(13, 139)
+        dgvOrder.Name = "dgvOrder"
+        dgvOrder.Size = New Size(440, 81)
+        dgvOrder.TabIndex = 10
+        ' 
+        ' colMeal
+        ' 
+        colMeal.HeaderText = "Meal Description"
+        colMeal.Name = "colMeal"
+        ' 
+        ' colQty
+        ' 
+        colQty.HeaderText = "Quantity"
+        colQty.Name = "colQty"
+        ' 
+        ' colPrice
+        ' 
+        colPrice.HeaderText = "Unit Price"
+        colPrice.Name = "colPrice"
+        ' 
+        ' colSubtotal
+        ' 
+        colSubtotal.HeaderText = "Subtotal"
+        colSubtotal.Name = "colSubtotal"
+        ' 
+        ' lblTotal
+        ' 
+        lblTotal.AutoSize = True
+        lblTotal.Location = New Point(8, 520)
+        lblTotal.Name = "lblTotal"
+        lblTotal.Size = New Size(34, 15)
+        lblTotal.TabIndex = 11
+        lblTotal.Text = "$0.00"
+        ' 
+        ' lblGST
+        ' 
+        lblGST.AutoSize = True
+        lblGST.Location = New Point(8, 535)
+        lblGST.Name = "lblGST"
+        lblGST.Size = New Size(34, 15)
+        lblGST.TabIndex = 12
+        lblGST.Text = "$0.00"
+        ' 
+        ' lblToPay
+        ' 
+        lblToPay.AutoSize = True
+        lblToPay.Location = New Point(8, 550)
+        lblToPay.Name = "lblToPay"
+        lblToPay.Size = New Size(34, 15)
+        lblToPay.TabIndex = 13
+        lblToPay.Text = "$0.00"
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(405, 539)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(75, 23)
+        btnExit.TabIndex = 14
+        btnExit.Text = "Exit Form"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(txtCustomerName)
+        GroupBox1.Controls.Add(txtAddress)
+        GroupBox1.Controls.Add(dtpOrderDate)
+        GroupBox1.Location = New Point(8, 27)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(473, 221)
+        GroupBox1.TabIndex = 15
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Customer Details"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(54, 129)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(67, 15)
+        Label4.TabIndex = 7
+        Label4.Text = "Order Date:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(54, 95)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(52, 15)
+        Label3.TabIndex = 6
+        Label3.Text = "Address:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(54, 66)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(64, 15)
+        Label2.TabIndex = 5
+        Label2.Text = "Full Name:"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(Label7)
+        GroupBox2.Controls.Add(Label6)
+        GroupBox2.Controls.Add(Label5)
+        GroupBox2.Controls.Add(dgvOrder)
+        GroupBox2.Controls.Add(txtQuantity)
+        GroupBox2.Controls.Add(txtPrice)
+        GroupBox2.Controls.Add(cmbMeal)
+        GroupBox2.Controls.Add(btnAdd)
+        GroupBox2.Controls.Add(btnDelete)
+        GroupBox2.Location = New Point(8, 254)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(473, 252)
+        GroupBox2.TabIndex = 16
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Order Details"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(12, 25)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(33, 15)
+        Label7.TabIndex = 13
+        Label7.Text = "Meal"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(278, 25)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(33, 15)
+        Label6.TabIndex = 12
+        Label6.Text = "Price"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(149, 25)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(53, 15)
+        Label5.TabIndex = 11
+        Label5.Text = "Quantity"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(492, 578)
+        Controls.Add(GroupBox2)
+        Controls.Add(Label1)
+        Controls.Add(btnExit)
+        Controls.Add(lblToPay)
+        Controls.Add(lblTime)
+        Controls.Add(lblGST)
+        Controls.Add(lblTotal)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dgvOrder, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblTime As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents txtCustomerName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents dtpOrderDate As DateTimePicker
+    Friend WithEvents cmbMeal As ComboBox
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents dgvOrder As DataGridView
+    Friend WithEvents colMeal As DataGridViewTextBoxColumn
+    Friend WithEvents colQty As DataGridViewTextBoxColumn
+    Friend WithEvents colPrice As DataGridViewTextBoxColumn
+    Friend WithEvents colSubtotal As DataGridViewTextBoxColumn
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblGST As Label
+    Friend WithEvents lblToPay As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+
+End Class
---
+++ 02-Automated Test/QuickDelightOrderForm v1.2/QuickDelightOrderForm/Form1.vb
@@ -0,0 +1,119 @@
+﻿Imports System.Windows.Forms
+
+Public Class Form1
+
+    ' ==========================================
+    ' SR 4.8 & 4.9: MAKING IT ACTUALLY WORK
+    ' ==========================================
+
+    ' 1. THE CLOCK
+    ' This just makes the time update every second (1000ms) so it's not stuck on 00:00:00 forever.
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lblTime.Text = DateTime.Now.ToString("HH:mm:ss")
+    End Sub
+
+    ' 2. THE ADD BUTTON (The big logic part)
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        ' VALIDATION (SR 4.9)
+        ' First, we gotta check if the user actually typed anything.
+        ' If they leave stuff blank and hit Add, the app crashes and that is NOT the vibe.
+        If cmbMeal.Text = "" Or txtQuantity.Text = "" Or txtPrice.Text = "" Then
+            MessageBox.Show("Please fill in all order fields!", "Error")
+            Return ' Stop right here, don't try to do math on empty air.
+        End If
+
+        Try
+            ' Grabbing the info from the boxes
+            Dim meal As String = cmbMeal.Text
+            ' Converting the text numbers into actual math numbers (Integer for qty, Decimal for money)
+            Dim qty As Integer = Integer.Parse(txtQuantity.Text)
+            Dim price As Decimal = Decimal.Parse(txtPrice.Text)
+
+            ' The Math: Quantity x Price = Subtotal
+            Dim subtotal As Decimal = qty * price
+
+            ' Shoving it all into the grid so we can see the order
+            dgvOrder.Rows.Add(meal, qty, price, subtotal)
+
+            ' Cleaning up the boxes so they are ready for the next order
+            txtQuantity.Clear()
+            txtPrice.Clear()
+
+            ' Update the total money at the bottom
+            CalculateTotals()
+
+        Catch ex As Exception
+            ' If they type "ABC" into the price box, this catches the error so the app doesn't explode.
+            MessageBox.Show("Please enter valid numbers for Quantity and Price.")
+        End Try
+    End Sub
+
+    ' 3. THE DELETE BUTTON
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        ' Check if they actually clicked a row before trying to delete
+        If dgvOrder.SelectedRows.Count > 0 Then
+            ' Loop through selected rows and yeet them
+            For Each row As DataGridViewRow In dgvOrder.SelectedRows
+                If Not row.IsNewRow Then
+                    dgvOrder.Rows.Remove(row)
+                End If
+            Next
+
+            ' Recalculate the money since we removed stuff
+            CalculateTotals()
+        End If
+    End Sub
+
+    ' 4. THE MATH HELPER
+    ' I made this a separate thing so I can use it for both Adding and Deleting without rewriting code.
+    Private Sub CalculateTotals()
+        Dim total As Decimal = 0
+
+        ' Go through every single row in the grid and add up the Subtotal column
+        For Each row As DataGridViewRow In dgvOrder.Rows
+            If row.Cells("colSubtotal").Value IsNot Nothing Then
+                total += Convert.ToDecimal(row.Cells("colSubtotal").Value)
+            End If
+        Next
+
+        ' Calculate the tax (15%)
+        Dim gst As Decimal = total * 0.15D
+        Dim toPay As Decimal = total + gst
+
+        ' Update the labels on the screen
+        ' The "C" thing formats it as Currency (puts the $ sign there automatically)
+        lblTotal.Text = $"Total: {total:C}"
+        lblGST.Text = $"GST (15%): {gst:C}"
+        lblToPay.Text = $"To Pay: {toPay:C}"
+    End Sub
+
+    ' 5. EXIT BUTTON
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Literally just closes the window.
+        Me.Close()
+    End Sub
+
+    ' ON STARTUP
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Make sure the clock starts ticking as soon as the app opens
+        Timer1.Start()
+    End Sub
+
+    ' --- GHOST CODE (Safety Nets) ---
+    ' These act like placeholders because I accidentally double-clicked some labels earlier.
+    ' If I delete these, the Designer gets confused and throws a tantrum.
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblTime.Click
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    End Sub
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+    End Sub
+    Private Sub dtpOrderDate_ValueChanged(sender As Object, e As EventArgs) Handles dtpOrderDate.ValueChanged
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        ' Redirects to the real button if needed
+        btnAdd_Click(sender, e)
+    End Sub
+
+End Class
